183d8ce03eb5728c17b927a8237c1af659879d0d
seunghun_5945 <tmdgns5945@oasis.inje.ac.kr>
2024-12-06 04:26:22 +0900

    test
    test

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 def draw_shapes():
-    # 1. 피라미드 별찍기
     print("1. 피라미드 별찍기")
     for i in range(5):
         print(" " * (4-i) + "*" * (2*i+1))
     print()
     
-    # 2. 사각형 그리기
     print("2. 사각형 그리기")
     for i in range(5):
         if i == 0 or i == 4:
@@ -14,7 +12,6 @@ def draw_shapes():
             print("*       *")
     print()
     
-    # 3. 하트 모양 그리기
     print("3. 하트 모양")
     for i in range(6):
         if i == 0:
