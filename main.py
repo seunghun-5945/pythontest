6c85c24e76ae2b4c51fca17629e2562deedaea42
seunghun_5945 <tmdgns5945@oasis.inje.ac.kr>
2024-12-06 10:47:00 +0900

    test
    test

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
-def draw_shapes():
-    # 1. 피라미드 별찍기
-    print("1. 피라미드 별찍기")
-    for i in range(5):
-        print(" " * (4-i) + "*" * (2*i+1))
-    print()
-    
-    # 2. 사각형 그리기
-    print("2. 사각형 그리기")
-    for i in range(5):
-        if i == 0 or i == 4:
-            print("* " * 5)
-        else:
-            print("*       *")
-    print()
-    
-    # 3. 하트 모양 그리기
-    print("3. 하트 모양")
-    for i in range(6):
-        if i == 0:
-            print("  *   *  ")
-        elif i == 1:
-            print("* * * * *")
-        elif i == 2:
-            print("* * * * *")
-        else:
-            print(" " * i + "*" * (9-2*i))
-    print()
+# 1. 기본 직각삼각형
+n = 5
+for i in range(n):
+    print('*' * (i + 1))
 
-if __name__ == "__main__":
-    draw_shapes()
+# 2. 역직각삼각형
+for i in range(n):
+    print('*' * (n - i))
+
+# 3. 피라미드
+for i in range(n):
+    print(' ' * (n - i - 1) + '*' * (2 * i + 1))
+
+# 4. 역피라미드
+for i in range(n):
+    print(' ' * i + '*' * (2 * (n - i) - 1))
+
+# 5. 마름모
+n = 3
+for i in range(n):
+    print(' ' * (n - i - 1) + '*' * (2 * i + 1))
+for i in range(n-2, -1, -1):
+    print(' ' * (n - i - 1) + '*' * (2 * i + 1))
