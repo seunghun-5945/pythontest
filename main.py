eefeb9c47a8de47b6c276a47894a40da7400cf8c
seunghun_lee <126228758+seunghun-5945@users.noreply.github.com>
2024-12-22 14:36:31 +0900

    pythontest

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 n = 5
+
+# 피라미드 형태
+for i in range(n):
+    print(' ' * (n - i - 1) + '*' * (2 * i + 1))
+
+# 역 피라미드 형태
+for i in range(n - 2, -1, -1):
+    print(' ' * (n - i - 1) + '*' * (2 * i + 1))
+for i in range(n - 2, -1, -1):
+    print(' ' * (n - i - 1) + '*' * (2 * i + 1))
+
+# 기본 피라미드 예제
+print("\n기본 피라미드:")
+for i in range(n):
+    print(' ' * (n - i - 1) + '*' * (i + 1))
+
+# 역 피라미드 예제
+print("\n역 피라미드:")
+for i in range(n, 0, -1):
+    print(' ' * (n - i) + '*' * i)
+
+# 다이아몬드 형태
+print("\n다이아몬드:")
 for i in range(n):
     print(' ' * (n - i - 1) + '*' * (2 * i + 1))
 for i in range(n - 2, -1, -1):
