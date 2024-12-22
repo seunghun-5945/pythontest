9fe9c7fcab49ed85b653df79f08873a725842fd3
seunghun_lee <126228758+seunghun-5945@users.noreply.github.com>
2024-12-22 21:36:11 +0900

    test1234

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 n = 5
 
 # 피라미드 형태
@@ -26,3 +28,19 @@ for i in range(n):
     print(' ' * (n - i - 1) + '*' * (2 * i + 1))
 for i in range(n - 2, -1, -1):
     print(' ' * (n - i - 1) + '*' * (2 * i + 1))
+
+# 직각삼각형 형태
+print("\n직각삼각형:")
+for i in range(1, n + 1):
+    print('*' * i)
+
+# 원 형태
+print("\n원:")
+radius = n // 2
+for y in range(-radius, radius + 1):
+    for x in range(-radius, radius + 1):
+        if math.isclose(x**2 + y**2, radius**2, abs_tol=0.5):
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')
+    print()
